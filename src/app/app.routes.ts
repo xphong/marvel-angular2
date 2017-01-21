81338d08a863357d4135dbf3e08980cf69385c14
Phong Huynh <xphong@gmail.com>
2017-01-21 16:01:19 -0500

    Add about to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { CharactersComponent } from './characters/characters.component';
+import { AboutComponent } from './about/about.component';
 
 export const ROUTES: Routes = [
-  { path: '', component: CharactersComponent }
+  { path: '', component: CharactersComponent },
+  { path: 'about', component: AboutComponent }
 ];
